95b473c33ebd1aa02a0dce282e6e3347a733c5c7
Idan17 <idanbartov@gmail.com>
2024-11-23 17:58:03 +0200

    get post by id route

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,37 +1,39 @@
 const Posts = require("../models/post.js");
 
 const createPost = async (req, res) => {
- try{
-     const post = await Posts.create({
-         title: req.body.title,
-         content: req.body.content,
-         owner: req.body.owner,
-        });
-     res.status(200).json(post);
-    } catch (error) {
-        res.status(500).send("Error creating post", error);
-    }
+  try {
+    const post = await Posts.create({
+      title: req.body.title,
+      content: req.body.content,
+      owner: req.body.owner,
+    });
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).send("Error creating post", error);
+  }
 };
 
 const getAllPosts = async (req, res) => {
-    try {
-      const posts = await Posts.find();
-      res.status(200).json(posts);
-    } catch (error) {
-      res.status(500).send("Error fetching posts", error);
-    }
-  };
+  try {
+    const posts = await Posts.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).send("Error fetching posts", error);
+  }
+};
 
 const getPostById = async (req, res) => {
-    try{
-        const post = await Posts.findById(req.params.id);
-        if(!post){
-            return res.status(404).send("Post not found");
-        }
-        res.status(200).send(post);
-    }catch(error){
-        res.status(500).send("Error fetching post", error);
+  const id = req.params.id;
+
+  try {
+    const post = await Posts.findById(id);
+    if (!post) {
+      return res.status(404).json({ message: "Post not found" });
     }
-}
+    res.status(200).json(post);
+  } catch (error) {
+    res.status(500).json({ message: "Error fetching comment", error });
+  }
+};
 
 module.exports = { createPost, getAllPosts, getPostById };
