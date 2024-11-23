5a8da42f5ac7dc7d21b623a0739feaad4804b077
Idan17 <idanbartov@gmail.com>
2024-11-23 17:45:43 +0200

    feat: getPostById controller

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,21 +1,37 @@
 const Posts = require("../models/post.js");
 
-const createPost = (req, res) => {
-  Posts.create({
-    title: req.body.title,
-    content: req.body.content,
-    owner: req.body.owner,
-  });
-  res.send("Post created");
+const createPost = async (req, res) => {
+ try{
+     const post = await Posts.create({
+         title: req.body.title,
+         content: req.body.content,
+         owner: req.body.owner,
+        });
+     res.status(200).json(post);
+    } catch (error) {
+        res.status(500).send("Error creating post", error);
+    }
 };
 
 const getAllPosts = async (req, res) => {
     try {
       const posts = await Posts.find();
-      res.send(posts);
+      res.status(200).json(posts);
     } catch (error) {
       res.status(500).send("Error fetching posts", error);
     }
   };
 
-module.exports = { createPost, getAllPosts };
+const getPostById = async (req, res) => {
+    try{
+        const post = await Posts.findById(req.params.id);
+        if(!post){
+            return res.status(404).send("Post not found");
+        }
+        res.status(200).send(post);
+    }catch(error){
+        res.status(500).send("Error fetching post", error);
+    }
+}
+
+module.exports = { createPost, getAllPosts, getPostById };
