417e201421596f2c224b166348e2f768aac4e50c
Idan17 <idanbartov@gmail.com>
2024-12-05 22:01:00 +0200

    fix CreatePost

--- src/controllers/post.js
+++ src/controllers/post.js
@@ -1,30 +1,33 @@
 const Posts = require("../models/post.js");
-const mongoose = require("mongoose");
 
 const createPost = async (req, res) => {
+  const { title, content, sender } = req.body;
+
+  if (!title || !content || !sender) {
+    return res
+      .status(400)
+      .json({ message: "Title, content and sender are required" });
+  }
+
   try {
     const post = await Posts.create({
-      title: req.body.title,
-      content: req.body.content,
-      sender: req.body.sender,
+      title,
+      content,
+      sender,
     });
-    res.status(200).json(post);
+    res.status(201).json(post);
   } catch (error) {
     res.status(500).send("Error creating post", error);
   }
 };
 
 const getPostById = async (req, res) => {
-  const { id } = req.params;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "Invalid id" });
-  }
+  const id = req.params.id;
 
   try {
     const post = await Posts.findById(id);
     if (!post) {
-      return res.status(404).json({ message: "Post is not found" });
+      return res.status(404).json({ message: "Post not found" });
     }
     res.status(200).json(post);
   } catch (error) {
@@ -48,11 +51,6 @@ const getPosts = async (req, res) => {
 const updatePost = async (req, res) => {
   const id = req.params.id;
   const { title, content, sender } = req.body;
-
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).json({ error: "Invalid id" });
-  }
-  
   try {
     const updatedPost = await Posts.findByIdAndUpdate(
       id,
